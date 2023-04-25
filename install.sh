52a2d941e441f6968851a098ef4fff3f1146d57a
DanielGrenehed <daniel.grenehed97@gmail.com>
2023-04-25 22:14:28 +0200

    installing dependencies

--- install.sh
+++ install.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
+sudo apt-get install -y nodejs universal-ctags ranger
 mkdir -p ~/.config
 mkdir -p ~/.config/nvim
 
