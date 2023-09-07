12054192a9cc40328770b00bb3aeb8c339040c62
DanielGrenehed <daniel.grenehed97@gmail.com>
2023-09-07 16:38:58 +0200

    fixed node and nvim install

--- install.sh
+++ install.sh
@@ -2,8 +2,10 @@
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
-curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash - &&\
-sudo apt-get install -y nodejs universal-ctags ranger
+
+sudo apt-get install -y snapd universal-ctags ranger
+sudo snap install node --classic
+sudo snap install --beta nvim --classic
 mkdir -p ~/.config
 mkdir -p ~/.config/nvim
 
