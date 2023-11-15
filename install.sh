cccbe40b40aae537c40e7f48880c51901b64ff2b
DanielGrenehed <daniel.grenehed97@gmail.com>
2023-11-15 19:48:09 +0100

    added gettext

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-sudo apt-get install -y snapd universal-ctags ranger make cmake
+sudo apt-get install -y snapd universal-ctags ranger make cmake gettext
 sudo snap install node --classic
 
 git clone https://github.com/neovim/neovim
