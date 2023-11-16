337a29bf531363a505eb3a6dd1e500a156eff802
DanielGrenehed <daniel.grenehed97@gmail.com>
2023-11-16 09:20:12 +0100

    removed snap, added nvm and tmux

--- install.sh
+++ install.sh
@@ -3,8 +3,13 @@
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-sudo apt-get install -y snapd universal-ctags ranger make cmake gettext
-sudo snap install node --classic
+sudo apt-get install -y universal-ctags ranger make cmake gettext tmux
+#sudo apt-get install snapd
+#sudo snap install node --classic
+
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+nvm install v16.20.2
 
 git clone https://github.com/neovim/neovim
 (cd neovim && make && sudo make install)
@@ -15,3 +20,5 @@ ln -f nvim/init.vim ~/.config/nvim/init.vim
 
 mkdir -p ~/.config/nvim/colors
 ln -f nvim/colors/sunbather.vim ~/.config/nvim/colors/sunbather.vim
+
+ln -f tmux/tmux.conf ~/.tmux.conf
