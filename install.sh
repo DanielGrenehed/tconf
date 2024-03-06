761d961053f6e48ab98fcde637b8f320da022566
DanielGrenehed <daniel.grenehed97@gmail.com>
2024-03-06 14:40:02 +0100

    linking before installing node

--- install.sh
+++ install.sh
@@ -1,24 +1,29 @@
 #!/bin/bash
 # install dependencies
 
-packages='universal-ctags ranger make cmake gettext tmux xclip'
+packages='universal-ctags ranger make cmake gettext tmux xclip gcc-multilib'
 
 if [ -x "$(command -v apt-get)" ]; then sudo apt-get install -y $packages
 elif [ -x "$(command -v brew)" ]; then brew install $packages
 else echo "Failed to install packages: Package manager not found! could not install ($packages)" >&2;fi
 # install node using nvm
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+echo "Linking"
+/bin/bash link.sh
+
 source ~/.bashrc
+echo "Installing node"
 nvm install v16.20.2
 
-# build and install neovim from source
+echo "Downloading neovim source"
 git clone https://github.com/neovim/neovim
+echo "Building neovim"
 (cd neovim && make && sudo make install)
 
-# install plug for nvim
+echo "Installing plug"
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 
-/bin/bash link.sh
-
+echo "Installing neovim plugins"
 nvim +PlugInstall
