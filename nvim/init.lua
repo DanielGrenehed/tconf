ec85ead29ab9dc2e2fc58c3c48603f9efa784cf7
DanielGrenehed <daniel.grenehed97@gmail.com>
2024-05-23 22:01:15 +0200

    alacritty windows fix

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,18 +1,16 @@
-
+-- plug fix
 local Plug = vim.fn['plug#']
 vim.call('plug#begin')
-
 Plug 'nvim-tree/nvim-web-devicons'
 Plug 'nvim-tree/nvim-tree.lua'
-
 Plug 'liuchengxu/vista.vim'
-
 Plug('neoclide/coc.nvim', {branch = 'release'})
 vim.call('plug#end')
 
 require('nvim-web-devicons')
 require('nvim-tree').setup()
 
+-- core vim config
 vim.cmd([[
 	set number
 	set relativenumber
@@ -23,6 +21,8 @@ vim.cmd([[
 vim.g.python3_host_prog = '/usr/local/bin/python3'
 vim.g.markdown_fenced_languages = {'html', 'python', 'cpp', 'c', 'rust', 'vim', 'go'}
 
+
+-- default mapping on all systems
 vim.cmd([[
 	nnoremap <silent> <F12> :NvimTreeToggle<CR>
 	nnoremap <silent> <F24> :Vista!!<CR>
@@ -30,48 +30,28 @@ vim.cmd([[
 	nnoremap <silent> <F6> :tabnew<CR>
 	nnoremap <silent> <F18> :vsplit<CR>
 	nnoremap <silent> <S-Del> :q<CR>
-
-	nnoremap <silent> <F1> :Files<CR>
-	nnoremap <silent> <F13> :GFiles<CR>
-
-	nnoremap <silent> ¶ :wincmd h<CR>
-	nnoremap <silent> ŧ :wincmd l<CR>
-	nnoremap <silent> đ :wincmd k<CR>
-	nnoremap <silent> ß :wincmd j<CR>
-
-	nnoremap <silent> Ŧ :vertical resize +1<CR>
-	nnoremap <silent> ® :vertical resize -1<CR>
-	nnoremap <silent> ª :resize +1<CR>
-	nnoremap <silent> § :resize -1<CR>
-
-	nnoremap <silent> ® :wincmd h<CR>
-	nnoremap <silent> þ :wincmd l<CR>
-	nnoremap <silent> đ :wincmd k<CR>
-	nnoremap <silent> ß :wincmd j<CR>
-
-	nnoremap <silent> ™ :vertical resize +1<CR>
-	nnoremap <silent> Þ :vertical resize -1<CR>
-	nnoremap <silent> ª :resize +1<CR>
-	nnoremap <silent> ẞ :resize -1<CR>
 ]])
+
 function sys_command(command) 
 	local handle = io.popen(command)
 	local result = handle:read("*a")
 	handle.close()
 	return result
 end 
-
 local virt = sys_command("systemd-detect-virt")
+local ostype = sys_command("echo $OSTYPE")
 
 
+
+-- per system keybindings
 if string.find(virt, "wsl") then
 	vim.cmd([[
-		nnoremap <silent> <C-r> :wincmd h<CR>
-		nnoremap <silent> <C-t> :wincmd l<CR>
-		nnoremap <silent> <C-f> :wincmd k<CR>
-		nnoremap <silent> <C-s> :wincmd j<CR>
+		nnoremap <silent> <C-S-Left> :wincmd h<CR>
+		nnoremap <silent> <C-S-Right> :wincmd l<CR>
+		nnoremap <silent> <C-S-Up> :wincmd k<CR>
+		nnoremap <silent> <C-S-Down> :wincmd j<CR>
  	]])
-elseif string.find(sys_command("echo $OSTYPE"), "darwin") then 
+elseif string.find(ostype, "darwin") then 
 	vim.cmd([[
 		nnoremap <silent> <M-Left> :wincmd h<CR>
 		nnoremap <silent> <M-Right> :wincmd l<CR>
@@ -84,7 +64,8 @@ elseif string.find(sys_command("echo $OSTYPE"), "darwin") then
 		nnoremap <silent> <M-S-Down> :resize -1<CR>
 
 	]])
-else 	
+else 
+	print("not wsl")
 	vim.cmd([[
 		nnoremap <silent> <M-Left> :wincmd h<CR>
 		nnoremap <silent> <M-Right> :wincmd l<CR>
--- readme.md
+++ readme.md
@@ -1,7 +1,10 @@
-# Terminal setup and configuration for unix systems
+# Terminal setup and configuration 
 
 ## Install
 Run the ```install.sh```, 
 this will link [vim config file](nvim/init.vim) and [.tmux.conf](tmux/tmux.conf) to this repo. 
-Then open ```vim``` and run ```:PlugInstall```
 
+### WSL on Windows
+
+#### Alacritty 
+I tend to use Alacritty terminal on all systems, but windows require a fix to get the mouse features to work correctly. The fix can be found [here](https://github.com/alacritty/alacritty/issues/1663#issuecomment-1917418514)
