8efa6e2f6f0dc47a687a741b8d8b4fdb3cca5396
DanielGrenehed <daniel@acczo.com>
2025-01-24 13:24:40 +0100

    Updated nvimtree config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,12 +3,23 @@ local Plug = vim.fn['plug#']
 vim.call('plug#begin')
 Plug 'nvim-tree/nvim-web-devicons'
 Plug 'nvim-tree/nvim-tree.lua'
-Plug 'liuchengxu/vista.vim'
+--Plug 'liuchengxu/vista.vim'
 Plug('neoclide/coc.nvim', {branch = 'release'})
 vim.call('plug#end')
 
 require('nvim-web-devicons')
-require('nvim-tree').setup()
+require('nvim-tree').setup({
+  view = {
+    number = true,
+    relativenumber = true,
+  },
+  actions = {
+    open_file = {
+      quit_on_open = true,
+    },
+  },
+})
+
 
 -- core vim config
 vim.cmd([[
@@ -16,16 +27,22 @@ vim.cmd([[
 	set relativenumber
 	set colorcolumn=80
 	highlight Normal ctermbg=NONE guibg=NONE
+
+	set tabstop=2
+	set shiftwidth=2
+	set expandtab
+	set smartindent
 ]])
 
 vim.g.python3_host_prog = '/usr/local/bin/python3'
 vim.g.markdown_fenced_languages = {'html', 'python', 'cpp', 'c', 'rust', 'vim', 'go'}
 
 
+-- map <silent> <F24> :Vista!!<CR>
+
 -- default mapping on all systems
 vim.cmd([[
 	nnoremap <silent> <F12> :NvimTreeToggle<CR>
-	nnoremap <silent> <F24> :Vista!!<CR>
 
 	nnoremap <silent> <F6> :tabnew<CR>
 	nnoremap <silent> <F18> :vsplit<CR>
@@ -43,6 +60,7 @@ vim.cmd([[
 	map <F23> :%!prettier --stdin-filepath %<CR> 
 ]])
 
+--[[
 function sys_command(command) 
 	local handle = io.popen(command)
 	local result = handle:read("*a")
@@ -52,7 +70,6 @@ end
 local virt = sys_command("systemd-detect-virt")
 local ostype = sys_command("echo $OSTYPE")
 
-
 -- per system 
 if string.find(virt, "wsl") then
 	print("wsl")
@@ -61,4 +78,4 @@ elseif string.find(ostype, "darwin") then
 else 
 	print("linux")
 end
-
+]]--
