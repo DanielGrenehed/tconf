ba760e530e190076e02d4e2f211677f23a065473
Daniel Amos Grenehed <weirddan@i7.local>
2026-03-14 23:38:43 +0100

    made installs optional

--- link.sh
+++ link.sh
@@ -5,8 +5,10 @@ echo "Linking tconf"
 mkdir -p ~/.config
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/nvim/colors
-mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/alacritty/themes
+
+# mkdir -p ~/.config/alacritty
+# mkdir -p ~/.config/alacritty/themes
+
 mkdir -p ~/.config/scripts
 
 ln -f nvim/init.lua ~/.config/nvim/init.lua
@@ -16,13 +18,14 @@ ln -f nvim/agignore ~/.agignore
 ln -f tmux/tmux.conf ~/.tmux.conf
 tmux source-file ~/.tmux.conf
 
-ln -f alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
-ln -f alacritty/themes/*.toml ~/.config/alacritty/themes/
+# ln -f alacritty/alacritty.toml ~/.config/alacritty/alacritty.toml
+# ln -f alacritty/themes/*.toml ~/.config/alacritty/themes/
 
 ln -f bash/bash_aliases ~/.bash_aliases
 ln -f bash/bashrc ~/.bashrc
 ln -f bash/bash_profile ~/.bash_profile
-ln -f bash/zshrc ~/.zshrc
+
+# ln -f bash/zshrc ~/.zshrc
 
 ln -f scripts/block_host.sh ~/.config/scripts/block_host.sh
 ln -f scripts/eject_cd.sh ~/.config/scripts/eject_cd.sh
