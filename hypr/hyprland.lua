b0f723decaec160d5517a34de3e08d53b0814aad
DanielGrenehed <daniel.grenehed97@gmail.com>
2026-08-04 09:37:36 +0200

    Added ctrl+shift+l/r to shift workspaces

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -304,6 +304,15 @@ for i = 1, 10 do
     hl.bind(mainMod .. " + SHIFT + " .. key,     hl.dsp.window.move({ workspace = i }))
 end
 
+hl.bind(
+  "CTRL + SHIFT + left", 
+  hl.dsp.focus({ workspace = "e-1" })
+)
+hl.bind(
+  "CTRL + SHIFT + right", 
+  hl.dsp.focus({ workspace = "e+1" })
+)
+
 -- Example special workspace (scratchpad)
 hl.bind(mainMod .. " + S",         hl.dsp.workspace.toggle_special("magic"))
 hl.bind(mainMod .. " + SHIFT + S", hl.dsp.window.move({ workspace = "special:magic" }))
